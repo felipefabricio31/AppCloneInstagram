dd6c88cd4a82817ea573d5b74591887a7085251c
Felipe Souza <felipe.fabricio.m3@gmail.com>
2019-05-10 00:28:02 -0300

    Trabalhando com Keyframes no angular. #258

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-acesso',
@@ -27,7 +27,17 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
         opacity: 0,
         transform: 'translate(80px, 0)'
       }),
-      animate('500ms 0s ease-in-out') // duração da transição, delay e aceleração 
+      //0 void ---X------------------X--X--X-------------------X- criado 1.5s//
+      animate('1.5s 0s ease-in-out', keyframes([
+        style({offset: 0.15, opacity:1, transform: 'translateX(0)' }),
+        style({offset: 0.86, opacity:1, transform: 'translateX(0)' }),
+        style({offset: 0.88, opacity:1, transform: 'translateY(-10px)' }),
+        style({offset: 0.90, opacity:1, transform: 'translateY(10px)' }),
+        style({offset: 0.92, opacity:1, transform: 'translateX(-90px)' }),
+        style({offset: 0.94, opacity:1, transform: 'translateX(90px)' }),
+        style({offset: 0.96, opacity:1, transform: 'translateY(-10px)' }),
+        style({offset: 0.98, opacity:1, transform: 'translateY(10px)' })
+      ])) // duração da transição, delay e aceleração 
     ])
   ])]
 })
@@ -43,8 +53,8 @@ export class AcessoComponent implements OnInit {
   ngOnInit() {
   }
 
-  public exibirPainel(event: string) : void {
-    this.cadastro = event  === 'cadastro' ? true : false
+  public exibirPainel(event: string): void {
+    this.cadastro = event === 'cadastro' ? true : false
   }
 
 }
