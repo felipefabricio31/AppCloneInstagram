deedb08db35918449171d2e500c2af481771b808
Felipe Souza <felipe.fabricio.m3@gmail.com>
2019-05-10 00:35:10 -0300

    Exemplos utilizando calbacks

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -29,14 +29,14 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
       }),
       //0 void ---X------------------X--X--X-------------------X- criado 1.5s//
       animate('1.5s 0s ease-in-out', keyframes([
-        style({offset: 0.15, opacity:1, transform: 'translateX(0)' }),
-        style({offset: 0.86, opacity:1, transform: 'translateX(0)' }),
-        style({offset: 0.88, opacity:1, transform: 'translateY(-10px)' }),
-        style({offset: 0.90, opacity:1, transform: 'translateY(10px)' }),
-        style({offset: 0.92, opacity:1, transform: 'translateX(-90px)' }),
-        style({offset: 0.94, opacity:1, transform: 'translateX(90px)' }),
-        style({offset: 0.96, opacity:1, transform: 'translateY(-10px)' }),
-        style({offset: 0.98, opacity:1, transform: 'translateY(10px)' })
+        style({ offset: 0.15, opacity: 1, transform: 'translateX(0)' }),
+        style({ offset: 0.86, opacity: 1, transform: 'translateX(0)' }),
+        style({ offset: 0.88, opacity: 1, transform: 'translateY(-10px)' }),
+        style({ offset: 0.90, opacity: 1, transform: 'translateY(10px)' }),
+        style({ offset: 0.92, opacity: 1, transform: 'translateX(-50px)' }),
+        style({ offset: 0.94, opacity: 1, transform: 'translateX(50px)' }),
+        style({ offset: 0.96, opacity: 1, transform: 'translateY(-10px)' }),
+        style({ offset: 0.98, opacity: 1, transform: 'translateY(10px)' })
       ])) // duração da transição, delay e aceleração 
     ])
   ])]
@@ -57,4 +57,10 @@ export class AcessoComponent implements OnInit {
     this.cadastro = event === 'cadastro' ? true : false
   }
 
+  public inicioDaAnimacao(): void {
+    console.log('inicio')
+  }
+  public fimDaAnimacao(): void {
+    console.log('fim')
+  }
 }
